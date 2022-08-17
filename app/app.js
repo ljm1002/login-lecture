00ad4af5221b09446f40d442a9084fdf07de1f4a
ljm <banjakk@naver.com>
2022-08-18 01:28:30 +0900

    수정

--- app/app.js
+++ app/app.js
@@ -7,10 +7,10 @@ const app = express();
 
 
 //라우팅
-const home = require("./routes/home");
+const home = require("./src/routes/home");
 
 //앱 세팅
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 
